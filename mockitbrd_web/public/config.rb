9c46879aa96f849f41755efcf3f2003cdbdedcf2
earlieclark <earl@mockitbrd.com>
2014-01-12 12:54:58 -0500

    I committed the change to the compass ruby config file to allow compass to watch all scss files

--- mockitbrd_web/public/config.rb
+++ mockitbrd_web/public/config.rb
@@ -6,12 +6,13 @@ css_dir = "css"
 sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
+add_import_path "js/app/templates/scss"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
